0b5eecc9ad859b797d3b23213c1dc148dd4a333b
Tiago Santos <tiago.s.1189@gmail.com>
2022-03-06 22:50:41 +0000

    material-ui setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,29 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { createTheme, ThemeProvider } from '@material-ui/core'
+import Head from 'next/head'
+
+const theme = createTheme();
+
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side')
+      jssStyles?.parentElement?.removeChild(jssStyles)
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>World Multiplayer</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default MyApp
