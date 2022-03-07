540b49ec702067f6ac780fde101c568664cb69b5
Tiago Santos <tiago.s.1189@gmail.com>
2022-03-07 21:53:46 +0000

    using Grid in Container

--- components/layout/Container/index.tsx
+++ components/layout/Container/index.tsx
@@ -1,6 +1,7 @@
 import { 
   Box,
-  Container as MuiContainer
+  Container as MuiContainer,
+  Grid
 } from "@material-ui/core"
 
 interface ContainerProps {
@@ -13,17 +14,29 @@ export const Container: React.FC<ContainerProps> = ({
   align = 'default',
 }) => (
   <MuiContainer>
-    {align == 'default' ? (
-      children
-    ) : (
-      <Box
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        height="100vh"
-      >
-        {children}
-      </Box>
-    )}
+    <Box
+      {
+        ...(align === "center" 
+          ? {
+            display: 'flex',
+            height: '100vh',
+            alignItems: 'center'
+          } 
+          : {})
+      }
+    >
+      <Grid container>
+        {align === 'center' && <Grid item xs={1} sm={2} md={3}></Grid>}
+        <Grid
+          item
+          xs={align === 'center' ? 10 : 12}
+          sm={align === 'center' ? 8 : 12}
+          md={align === 'center' ? 6 : 12}
+        >
+          {children}
+        </Grid>
+        {align === 'center' && <Grid item xs={1} sm={2} md={3}></Grid>}
+      </Grid>
+    </Box>
   </MuiContainer>
 )
